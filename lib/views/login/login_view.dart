cdc0f54b540ac36ec7519ef45e3b36c0b6d0a44b
Anhelina Kruk <krukanhelina06@gmail.com>
2024-10-23 11:02:09 +0200

    creating login view

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -1,5 +1,7 @@
+import 'package:dsw53518/utils/my_images.dart';
 import 'package:flutter/material.dart';
 
+import '../../utils/my_colors.dart';
 import '../register/register_view.dart';
 
 class LoginView extends StatelessWidget {
@@ -7,16 +9,37 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Open route'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const RegisterView()),
-            );
-          },
+    return SafeArea(
+      child: Scaffold(
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            children: [
+              const SizedBox(height: 62,),
+              Image.asset(MyImages.logo),
+              Text(
+                'Sign In',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                  color: MyColors.purpleColor,
+                ),
+              ),
+              GestureDetector(
+                  onTap: () => print('xxx'),
+                  child:  Text('Sign up')
+              ),
+              ElevatedButton(
+                child: const Text('Open route'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const RegisterView()),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/views/register/register_view.dart
+++ lib/views/register/register_view.dart
@@ -5,13 +5,15 @@ class RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Go back!'),
+          ),
         ),
       ),
     );
