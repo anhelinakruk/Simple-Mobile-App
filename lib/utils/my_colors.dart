4730d75f8d46d490f6555f3e70e7315c6d1a7aae
Anhelina Kruk <krukanhelina06@gmail.com>
2024-11-04 20:18:39 +0100

    added sign-in view

--- lib/utils/my_colors.dart
+++ lib/utils/my_colors.dart
@@ -3,7 +3,10 @@ import 'dart:ui';
 class MyColors {
   static final blackColor = HexColor('OEOEOE');
   static final whiteColor = HexColor('FFFFFF');
-  static final purpleColor = HexColor('471AA0');
+  static final purpleColor = HexColor('9747FF');
+  static final transparentBlack = Color.fromRGBO(0, 0, 0, 0.3);
+  static final darkPurpleColor = HexColor('471AA0');
+  static final lightPurpleColor = HexColor('BB84E8');
 }
 
 class HexColor extends Color {
